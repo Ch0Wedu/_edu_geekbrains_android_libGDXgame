ce02d5811a93c7395b5a1ac8e213a189f49c4aab
Ch0W <ch0wru@gmail.com>
2018-09-15 01:34:51 +0300

    Project created

---
+++ android/src/ru/syskaev/edu/geekbrains/android/libgdxgame/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package ru.syskaev.edu.geekbrains.android.libgdxgame;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ru.syskaev.edu.geekbrains.android.libgdxgame.Game;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Game(), config);
+	}
+}
---
+++ desktop/src/ru/syskaev/edu/geekbrains/android/libgdxgame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.syskaev.edu.geekbrains.android.libgdxgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.syskaev.edu.geekbrains.android.libgdxgame.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
